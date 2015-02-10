444cad1a059e21ab065ad48d6d8d574b5c6f4158
Andrew Do <do@berkeley.edu>
2015-02-10 13:00:40 -0800

    quickfix

--- hw1/stat240-hw1.R
+++ hw1/stat240-hw1.R
@@ -35,16 +35,16 @@ SampleFromBox <- function(x, size, replace= TRUE, prob = NULL) {
   }
 }
 
-StandardError <- function(x) {
+StandardError <- function(x, n) {
   # Computes the standard error
   # Arguments:
   #   x - a vector of ticket values from the box model
   #   n - sample size
   # Returns
-  #   standard error of a ticket in box model
+  #   standard error of the sample mean in the box model
   
   N <- length(x) # Number of tickets in box
-  se <- sqrt((N-1)/N) * sd(x)
+  se <- sqrt((N-1)/N/n) * sd(x)
   
   return(se)
 }
@@ -54,24 +54,24 @@ StandardError <- function(x) {
 tickets1 <- c(rep(0, 7), rep(1, 5))
 sample1 <- SampleFromBox(x = tickets1, size = 100000, replace = TRUE)
 mean1 <- mean(tickets1)
-se1 <- StandardError(tickets1)
+se1 <- StandardError(tickets1, 6)
 
 # Problem 2
 tickets2 <- c(rep(0, 98), rep(1, 2))
 sample2 <- SampleFromBox(x = tickets2, size = 100000, replace = TRUE)
 mean2 <- mean(tickets2)
-se2 <- StandardError(tickets2)
+se2 <- StandardError(tickets2, 6)
 
 # Problem 3
 tickets3 <- 1:12
 sample3 <- SampleFromBox(x = tickets3, size = 100000, replace = TRUE)
 mean3 <- mean(tickets3)
-se3 <- StandardError(tickets3)
+se3 <- StandardError(tickets3, 6)
 
 # Problem 4
 tickets4 <- c(1:11, 30)
 sample4 <- SampleFromBox(x = tickets4, size = 100000, replace = TRUE)
 mean4 <- mean(tickets4)
-se4 <- StandardError(tickets4)
+se4 <- StandardError(tickets4, 6)
 
 # Extra Credit
