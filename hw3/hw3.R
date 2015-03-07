4276e467ab40b8fa447e41ab7f8b7b87f1a4670a
kellieotto <kellieotto@berkeley.edu>
2015-03-06 16:30:02 -0800

    Fixed buggy code and prob 2 soln

--- hw3/hw3.R
+++ hw3/hw3.R
@@ -1,3 +1,5 @@
+set.seed(50)
+
 compute_test_stats <- function(x, y){
   # Computes difference in means and t statistic for tr. group x and control group y
   n <- length(x); m <- length(y);
@@ -15,7 +17,7 @@ perm_test_mean <- function(x, y, iters = 10000){
   # Output
   # TestStats = vector containing difference in means and the t-statistic
   # Pvalue = vector with lower, upper, and two-tailed p-values
-
+  n <- length(x)
   box <- c(x, y)
   obs <- compute_test_stats(x, y)
   distr <- replicate(iters, {
@@ -26,39 +28,28 @@ perm_test_mean <- function(x, y, iters = 10000){
   })
   pval_diffmeans <- c("upper" = sum(distr[1,] >= obs[1])/iters, "lower" = sum(distr[1,] <= obs[1])/iters, "both" = sum(abs(distr[1,]) >= abs(obs[1]))/iters)
   pval_t <- c("upper" = sum(distr[2,] >= obs[2])/iters, "lower" = sum(distr[2,] <= obs[2])/iters, "both" = sum(abs(distr[2,]) >= abs(obs[2]))/iters)
-  return(list("TestStats" = obs, distr, "MeansPvalue" = pval_diffmeans, "tPvalue" = pval_t))
+  return(list("TestStats" = obs, "distr" = distr, "MeansPvalue" = pval_diffmeans, "tPvalue" = pval_t))
 }
 
 
 
 
 ### Problem 2: Construct a hypothetical dataset (with at least 3 data points in treatment and at least 3 in control) for which the p-value of a permutation test based on X ̄ is smaller than the p-value of a permutation test based on t.
-x <- c(-5:5, 100, 1000)
-y <- -10:10
-res1 <-perm_test_mean(x, y)
-res1$tPvalue
-#upper  lower   both
-#0.0162 0.9838 0.3393
-res1$MeansPvalue
-#upper  lower   both
-#0.1492 0.8508 0.1492
-#res1$TestStats
+tr <- rnorm(5, mean = 0, sd = 20)
+# [1]  10.9933979 -16.8320749   0.6599587  10.4829942 -34.5520822
+ctrl <- rnorm(100, mean = 0, sd = 1)
+res2 <- perm_test_mean(tr, ctrl)
+res2$MeansPvalue
+#upper lower  both
+#0.95  0.05  0.05
+#
+res2$tPvalue
+#upper lower  both
+#0.861 0.139 0.559
+res2$TestStats
 #diff_means          t
-#84.615385   1.103447
+#-5.7512729 -0.6559599
+
 
 ### Problem 3: Construct a hypothetical dataset (with at least 3 data points in treatment and at least 3 in control) for which the p-value of a permutation test based on t is smaller than the p-value of a permutation test based on X ̄.
-x <- rnorm(20, mean = 0, sd = 1)
-y <- rnorm(5, mean = 0, sd = 20)
-res1 <- perm_test_mean(x, y)
-# $MeansPvalue
-# upper  lower   both
-# 1.0000 0.0000 0.0278
-#
-# $tPvalue
-# upper  lower   both
-# 0.6697 0.3303 0.3926
-# this is probably a bad example - let's think of another
-
-
-
 
