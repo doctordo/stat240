fd653fd053248f7195e7778dc03d1a65c70f30ab
Andrew Do <do@berkeley.edu>
2015-03-09 23:59:00 -0700

    worked on problem 3

--- hw3/hw3.R
+++ hw3/hw3.R
@@ -4,19 +4,24 @@ compute_test_stats <- function(x, y){
   # Computes difference in means and t statistic for tr. group x and control group y
   n <- length(x); m <- length(y);
   diff_means <- mean(x) - mean(y)
-  t <- diff_means/sqrt(var(x)/n + var(y)/m) # Do we need to multiply variance by n-1/n and m-1/m?
+  # Do we need to multiply variance by n-1/n and m-1/m?
+  # Andrew - I don't think so, we are estimating the variance
+  #   in a t-test
+  t <- diff_means/sqrt(var(x)/n + var(y)/m) 
   return(c("diff_means" = diff_means, "t" = t))
 }
 
 perm_test_mean <- function(x, y, iters = 10000){
-  # Input
-  # x = vector of observations for treatment group
-  # y = vector of observations for control group
-  # iters = number of permutations
+  # Performs permutation tests on the difference of means
+  #   and the t-statistic
+  # Input:
+  #   x - vector of observations for treatment group
+  #   y - vector of observations for control group
+  #   iters - number of permutations
 
-  # Output
-  # TestStats = vector containing difference in means and the t-statistic
-  # Pvalue = vector with lower, upper, and two-tailed p-values
+  # Output:
+  #   TestStats - vector containing difference in means and the t-statistic
+  #   Pvalue - vector with lower, upper, and two-tailed p-values
   n <- length(x)
   box <- c(x, y)
   obs <- compute_test_stats(x, y)
@@ -26,15 +31,28 @@ perm_test_mean <- function(x, y, iters = 10000){
     ynew <- shuffle[-(1:n)]
     compute_test_stats(xnew, ynew)
   })
-  pval_diffmeans <- c("upper" = sum(distr[1,] >= obs[1])/iters, "lower" = sum(distr[1,] <= obs[1])/iters, "both" = sum(abs(distr[1,]) >= abs(obs[1]))/iters)
-  pval_t <- c("upper" = sum(distr[2,] >= obs[2])/iters, "lower" = sum(distr[2,] <= obs[2])/iters, "both" = sum(abs(distr[2,]) >= abs(obs[2]))/iters)
-  return(list("TestStats" = obs, "distr" = distr, "MeansPvalue" = pval_diffmeans, "tPvalue" = pval_t))
+  
+  pval_diffmeans <- c("upper" = sum(distr[1,] >= obs[1])/iters, 
+                      "lower" = sum(distr[1,] <= obs[1])/iters, 
+                      "both" = sum(abs(distr[1,]) >= abs(obs[1]))/iters)
+  
+  pval_t <- c("upper" = sum(distr[2,] >= obs[2])/iters, 
+              "lower" = sum(distr[2,] <= obs[2])/iters, 
+              "both" = sum(abs(distr[2,]) >= abs(obs[2]))/iters)
+  
+  return(list("TestStats" = obs, 
+              "distr" = distr, 
+              "MeansPvalue" = pval_diffmeans, 
+              "tPvalue" = pval_t))
 }
 
 
 
 
-### Problem 2: Construct a hypothetical dataset (with at least 3 data points in treatment and at least 3 in control) for which the p-value of a permutation test based on X ̄ is smaller than the p-value of a permutation test based on t.
+### Problem 2: Construct a hypothetical dataset 
+# (with at least 3 data points in treatment and at least 3 in control) 
+# for which the p-value of a permutation test based on X ̄ is smaller
+# than the p-value of a permutation test based on t.
 tr <- rnorm(5, mean = 0, sd = 20)
 # [1]  10.9933979 -16.8320749   0.6599587  10.4829942 -34.5520822
 ctrl <- rnorm(100, mean = 0, sd = 1)
@@ -51,18 +69,46 @@ res2$TestStats
 #-5.7512729 -0.6559599
 
 
-### Problem 3: Construct a hypothetical dataset (with at least 3 data points in treatment and at least 3 in control) for which the p-value of a permutation test based on t is smaller than the p-value of a permutation test based on X ̄.
 
-# this sort of works but it's not a "HUGE" difference. Let's play with it more; increasing variability in the ctrl group helps
-tr_3 <- rnorm(10, mean = 2, sd = 1)
-ctrl_3 <- rnorm(20, mean = 0, sd = 10)
-res3 <- perm_test_mean(tr_3, ctrl_3)
+### Problem 3: Construct a hypothetical dataset 
+# (with at least 3 data points in treatment and at least 3 in control) 
+# for which the p-value of a permutation test based on t is smaller than 
+# the p-value of a permutation test based on X ̄.
+
+# this sort of works but it's not a "HUGE" difference. Let's play with it more; 
+# increasing variability in the ctrl group helps
+
+# Andrew - This always gives a higher p-value to the mean test with the mean of
+# the ratio of the p-values at 4 and median at 2.  It took roughly an hour to run 
+# 1000 simulations for the p-values.
+
+tr_3 <- rnorm(10, mean = 0, sd = 1)
+ctrl_3 <- rnorm(1000, mean = 1, sd = 100)
+res3 <- perm_test_mean(tr_3, ctrl_3, iters = 10000)
+res3$MeansPvalue[3] / res3$tPvalue[3]
 res3$TestStats
 res3$tPvalue
 res3$MeansPvalue
 
+SimulateP <- function(iterations = 1000) {
+  p <- rep(NA, iterations)
+  for (i in 1:iterations) {
+    tr <- rnorm(10, mean = 0, sd = 1)
+    ctrl <- rnorm(1000, mean = 1, sd = 100)
+    res <- perm_test_mean(tr, ctrl, iters = 10000)
+    p[i] <- res$MeansPvalue[3] / res$tPvalue[3]
+  }
+  return(p)
+}
 
-### Problem 4: Construct a hypothetical dataset (with at least 3 data points in treatment and at least 3 in control) for which the p-value of a permutation test based on X ̄ is smaller than the p-value of a standard t test.
+ggplot(data.frame(p=p)) + geom_histogram(aes(x=p), binwidth=1) + xlim(c(1,25))
+mean(p)
+median(p)
+
+### Problem 4: Construct a hypothetical dataset 
+# (with at least 3 data points in treatment and at least 3 in control) 
+# for which the p-value of a permutation test based on X ̄ is smaller
+# than the p-value of a standard t test.
 
 tr_4 <- c(-10:10, 500, 1000, 2000, 5000)
 ctrl_4 <- rep(0:5, 5)
