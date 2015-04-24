cc404641e9931f36cec458068fc06fd57e20632e
kellieotto <kellieotto@berkeley.edu>
2015-04-24 11:04:48 -0700

    Write up hw4 q1 done

--- hw4/hw4_simulations.R
+++ hw4/hw4_simulations.R
@@ -27,6 +27,7 @@ Y <- c(1.9, -0.3, 2.8, -0.7, 1.6, -2.4)
 permutation_test(X, Y, normal_approx=TRUE)
 # [1] 0.4505772 -- matches
 permutation_test(X, Y, normal_approx=FALSE, L=1000000)
+# [1] 0.454514
 
 wilcoxon_rank_sum_test(X, Y, normal_approx=TRUE)
 # [1] 0.5319069 -- matches
@@ -51,15 +52,24 @@ X <- c(0.5, 2.4, -4.1, 5.9, 3.7, 3.6)
 Y <- c(2.7, 0.8, 1.6, 5.7, 4.3, 0.3)
 
 permutation_test(X, Y, normal_approx=TRUE)
+# [1] 0.6393479
 permutation_test(X, Y, normal_approx=FALSE, L=1000000)
+# [1] 0.626417
 
-wilcoxon_rank_sum_test(X, Y, normal_approx=TRUE)
+  wilcoxon_rank_sum_test(X, Y, normal_approx=TRUE)
+# [1] 0.5319069
 wilcoxon_rank_sum_test(X, Y, normal_approx=FALSE, L=1000000)
+# [1] 0.531804
 
-paired_permutation_test(X, Y, normal_approx = TRUE)
+  paired_permutation_test(X, Y, normal_approx = TRUE)
+# [1] 0.6826982
 paired_permutation_test(X, Y, normal_approx = FALSE, L=1000000)
+# [1] 0.626229
 
-sign_test(X,Y)
+  sign_test(X,Y)
+# [1] 0.65625
 
-wilcoxon_signed_rank_test(X, Y, normal_approx = TRUE)
+  wilcoxon_signed_rank_test(X, Y, normal_approx = TRUE)
+# [1] 0.6625066
 wilcoxon_signed_rank_test(X, Y, normal_approx = FALSE, L=1000000)
+# [1] 0.655774
