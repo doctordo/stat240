c6ec141806bc1af9b43e8f0166136a131890714b
kellieotto <kellieotto@berkeley.edu>
2015-04-23 22:57:38 -0700

    Complete code for q1

--- hw4/hw4_simulations.R
+++ hw4/hw4_simulations.R
@@ -19,7 +19,8 @@ set.seed(1337)
 
 source("hw4_functions.R")
 
-############# Problem 1: test the functions
+############# Problem 1: test the functions.
+### Test set 1
 X <- c(-1.0, 1.7, -2.0, 0.6, 0.9, 3.5)
 Y <- c(1.9, -0.3, 2.8, -0.7, 1.6, -2.4)
 
@@ -43,3 +44,22 @@ wilcoxon_signed_rank_test(X, Y, normal_approx = TRUE)
 # [1] 0.5
 wilcoxon_signed_rank_test(X, Y, normal_approx = FALSE, L=1000000)
 # [1] 0.499427
+
+
+### Second test set
+X <- c(0.5, 2.4, -4.1, 5.9, 3.7, 3.6)
+Y <- c(2.7, 0.8, 1.6, 5.7, 4.3, 0.3)
+
+permutation_test(X, Y, normal_approx=TRUE)
+permutation_test(X, Y, normal_approx=FALSE, L=1000000)
+
+wilcoxon_rank_sum_test(X, Y, normal_approx=TRUE)
+wilcoxon_rank_sum_test(X, Y, normal_approx=FALSE, L=1000000)
+
+paired_permutation_test(X, Y, normal_approx = TRUE)
+paired_permutation_test(X, Y, normal_approx = FALSE, L=1000000)
+
+sign_test(X,Y)
+
+wilcoxon_signed_rank_test(X, Y, normal_approx = TRUE)
+wilcoxon_signed_rank_test(X, Y, normal_approx = FALSE, L=1000000)
