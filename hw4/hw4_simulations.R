bf63a0bfc029ec6fc821722c66755e366eba13e3
kellieotto <kellieotto@berkeley.edu>
2015-04-23 22:19:02 -0700

    Start working on hw4 q1

---
+++ hw4/hw4_simulations.R
@@ -0,0 +1,32 @@
+################################################################################
+
+# Stat 240 - Non-parametric Statistics
+# Homework 4
+# Due: 5/1/15
+
+# Rebecca Barter
+# Andrew Do
+# Kellie Ottoboni
+
+################################################################################
+
+# Libraries and seed
+library(dplyr, warn.conflicts = FALSE, quietly=TRUE)
+library(ggplot2)
+library(xtable)
+library(gridExtra)
+set.seed(1337)
+
+source("hw4_functions.R")
+
+############# Problem 1: test the functions
+X <- c(-1.0, 1.7, -2.0, 0.6, 0.9, 3.5)
+Y <- c(1.9, -0.3, 2.8, -0.7, 1.6, -2.4)
+permutation_test(X, Y, normal_approx=TRUE)
+# [1] 0.4528333 -- doesn't match
+permutation_test(X, Y, normal_approx=FALSE, L=1000000)
+
+wilcoxon_rank_sum_test(X, Y, normal_approx=TRUE)
+# [1] 0.5319069 -- matches
+wilcoxon_rank_sum_test(X, Y, normal_approx=FALSE)
+
